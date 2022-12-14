8f88cf7898629564ce035c7a40ae93a9dd76700d
Luís Gomes <luispegasgomes@gmail.com>
2022-12-14 21:49:53 +0000

    almost finished modules

--- controllers/book_modules.controller.js
+++ controllers/book_modules.controller.js
@@ -15,9 +15,6 @@ exports.create = async (req, res) => {
 
 
         // add module to found book (using a mixin)
-        // ERROR
-        
-        await book.addModule(newModule);
         
         
         res.status(201).json({
@@ -25,12 +22,110 @@ exports.create = async (req, res) => {
             URL: `/books/${req.params.bookID}/modules/${newModule.id}`
         });
     } catch (err) {
-        // console.log(err.name) // err.name === 'SequelizeValidationError'
-        if (err instanceof ValidationError)
-            res.status(400).json({ success: false, msg: err.errors.map(e => e.message) });
-        else
-            res.status(500).json({
-                success: false, msg: err.message || "Some error occurred while creating the tutorial."
-            });
+        
+        console.log(err);
+        return res.status(400).send('erro')
     };
 }
+
+exports.findAll = async (req, res) => {
+    try {
+        // try to find the book, given its ID
+        let book = await Book.findByPk(req.params.bookID, {
+            //include: Module
+            include: [{
+                model: Module,
+                attributes: ['id', 'module_name', 'BookId']
+            }]
+        })
+        if (book === null)
+            return res.status(404).json({
+                success: false, msg: `Cannot find any book with ID ${req.params.bookID}.`
+            });
+
+        res.status(200).json({
+            success: true, book: book
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false, msg: err.message || "Some error occurred while retrieving the book."
+        })
+    }
+}
+
+exports.delete = async (req, res) => {
+    try {
+        let book = await Book.findByPk(req.params.bookID)
+        if (book === null)
+            return res.status(404).json({
+                success: false, msg: `Cannot find any book with ID ${req.params.bookID}.`
+            });
+
+        let moduleBook = await Module.findByPk(req.params.idModule)
+        if (moduleBook === null)
+            return res.status(404).json({
+                success: false, msg: `Cannot find any module with ID ${req.params.idModule}.`
+            });
+
+
+        let result = await Module.destroy({ where: { id: req.params.idModule } })
+        // console.log(result)
+
+        if (result == 1) // the promise returns the number of deleted rows
+            return res.status(200).json({
+                success: true, msg: `Module with id ${req.params.idModule} was successfully deleted!`
+            });
+        // no rows deleted -> no book was found
+        res.status(404).json({
+            success: false, msg: `Cannot find any book module with ID ${req.params.idModule}.`
+        });
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false, msg: `Error deleting book module with ID ${req.params.idModule}.`
+        });
+    };
+};
+
+exports.update = async (req, res) => {
+    try {
+        // since Sequelize update() does not distinguish if a book exists, first let's try to find one
+        let book = await Book.findByPk(req.params.bookID);
+        if (book === null)
+            return res.status(404).json({
+                success: false, msg: `Cannot find any book with ID ${req.params.bookID}.`
+            });
+
+        let moduleBook = await Module.findByPk(req.params.idModule)
+        if (moduleBook === null)
+            return res.status(404).json({
+                success: false, msg: `Cannot find any module with ID ${req.params.idModule}.`
+            });
+
+        if (!req.body.moduleName) {
+            return res.status(404).json({
+                success: false, msg: `No module name found.`
+            });
+        }
+        // obtains only a single entry from the table, using the provided primary key
+        let affectedRows = await Module.update({ module_name: req.body.moduleName }, { where: { id: req.params.idModule } })
+        console.log(affectedRows)
+        if (affectedRows[0] === 0) // check if the book was updated (returns [0] if no data was updated)
+            return res.status(200).json({
+                success: true, msg: `No updates were made on module with ID ${req.params.idModule}.`
+            });
+
+        res.json({
+            success: true,
+            msg: `Module with ID ${req.params.idModule} was updated successfully.`
+        });
+    }
+    catch (err) {
+        if (err instanceof ValidationError)
+            return res.status(400).json({ success: false, msg: err.errors.map(e => e.message) });
+        res.status(500).json({
+            success: false, msg: `Error retrieving module with ID ${req.params.idModule}.`
+        });
+    };
+};
--- routes/book_modules.routes.js
+++ routes/book_modules.routes.js
@@ -1,17 +1,62 @@
 const express = require('express');
 const bookModulesController = require("../controllers/book_modules.controller");
-
-
+const {body, validationResult, param} = require("express-validator");
 // express router
-let router = express.Router({ mergeParams: true });
+let router = express.Router({
+    mergeParams: true
+});
 
 router.route('/')
-    .post(bookModulesController.create);
+    .get(bookModulesController.findAll);
 
+router.post("/", [
+    body("moduleName").trim().notEmpty().withMessage("Insert the module name!"),
+    param("bookID").trim().notEmpty().withMessage("Insert the module name!").isNumeric().withMessage("Insert an id!"),
+], function (req, res) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        bookModulesController.create(req, res);
+    } else {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+});
+
+router.delete("/:idModule", [
+    param("bookID").trim().notEmpty().withMessage("Insert the id!").isNumeric().withMessage("Insert an id!"),
+    param("idModule").trim().notEmpty().withMessage("Insert the id!").isNumeric().withMessage("Insert an id!"),
+], function (req, res) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        bookModulesController.delete(req, res);
+    } else {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+});
+
+router.patch("/:idModule", [
+    body("moduleName").trim().notEmpty().withMessage("Insert the module name!"),
+    param("bookID").trim().notEmpty().withMessage("Insert the id!").isNumeric().withMessage("Insert an id!"),
+    param("idModule").trim().notEmpty().withMessage("Insert the id!").isNumeric().withMessage("Insert an id!"),
+], function (req, res) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        bookModulesController.update(req, res);
+    } else {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+});
 
 router.all('*', function (req, res) {
     //send an predefined error message 
-    res.status(404).json({ message: 'MODULES: what???' });
+    res.status(404).json({
+        message: 'MODULES: what???'
+    });
 })
 
-module.exports = router;    
+module.exports = router;
